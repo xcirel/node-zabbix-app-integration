91133e19713d892b379736b830ce8c90db991365
Eric Santos <eric.santos@newrizon.global>
2023-06-23 09:56:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const zabbixSender = require('zabbix-nodejs');
+
+
+// Middleware para o parsing do corpo das requisições
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/test', (req, res) => { 
+  zabbixSender('send.email', 1)
+  res.status(200).json({ message: 'Hello DevOps' })
+});
+
+app.get('/error', (_req, res) => res.status(400).send('Status: :('));
+app.get('/server_error', (_req, res) => res.status(500).send('Status: :('));
+
+app.get('/exception', (req, res) => {
+  zabbixSender('user.login', 1)
+  throw new Error('Parameter is not a number!');
+});
+
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
